18b3a352482fbfcd1faa2ac10d02d47d1002cc4b
Mayank <mittalmayank.bvm@gmail.com>
2020-06-15 20:52:04 +0530

    Time Series project

--- Scripts/time_series_superstore.py
+++ Scripts/time_series_superstore.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 path = "E:/MyWorkStation/MM Work/Datasets/Superstore.xls"
 import datetime as dt
-df = pd.read_excel(path,usecols=[''])
+df = pd.read_excel(path,usecols=['Order Date','Category','Sales'])
 
-for values in df.Category.value_counts().index:
-    values = df[df.Category == values]
+furniture = df[df.Category == 'Furniture'].set_index('Order Date')
+furniture.drop('Category',inplace=True,axis=1)
+furniture_resample = furniture.resample('M').mean()
+
+plt.style.use('dark_background')
+furniture_resample.plot(figsize=(14,6))
