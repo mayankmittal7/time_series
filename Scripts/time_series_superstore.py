b1a2bd7829a7281e2908734118988e47f8fd9b5e
Mayank <mittalmayank.bvm@gmail.com>
2020-06-15 21:24:53 +0530

    Added graph for all 3 categories

--- Scripts/time_series_superstore.py
+++ Scripts/time_series_superstore.py
@@ -11,3 +11,20 @@ furniture_resample = furniture.resample('M').mean()
 
 plt.style.use('dark_background')
 furniture_resample.plot(figsize=(14,6))
+
+Office_Supplies = df[df.Category == 'Office Supplies'].set_index('Order Date')
+Office_Supplies.drop('Category',inplace=True,axis=1)
+Office_Supplies_resample = Office_Supplies.resample('M').mean()
+
+Technology = df[df.Category == 'Technology'].set_index('Order Date')
+Technology.drop('Category',inplace=True,axis=1)
+Technology_resample = Technology.resample('M').mean()
+
+plt.plot(furniture_resample,'b-',label='furniture')
+plt.plot(Office_Supplies_resample,'r-',label='office')
+plt.plot(Technology_resample,'g-',label='technology')
+plt.xlabel('Dates')
+plt.ylabel('Category')
+plt.title('Sales of different Categories')
+plt.legend()
+
